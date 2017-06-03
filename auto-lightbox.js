77de7ce264379130e7b38e09af0a10c745a39a7d
Paul Wardrip <pwarrdrip@gmail.com>
2017-06-02 20:26:07 -0400

    Reworked to not require jQuery ... untested!

--- auto-lightbox.js
+++ auto-lightbox.js
@@ -1,10 +1,12 @@
 var AutoLightBox = function(elem) {
-    var $box = $(document.createElement("div"));
-    var $window = $(window);
-    var $body = $("body");
+    var box = document.createElement("div");
+    //var $window = $(window);
+    //var $body = $("body");
     var attached = false;
-    var $parent;
+    //var $parent;
+    var parent;
     var visible = false;
+    
 
     var control;
 
@@ -20,26 +22,30 @@ var AutoLightBox = function(elem) {
     }
 
     var show = function () {
-        $box.fadeTo(200, .5);
+        if ($) {
+            $(box).fadeTo(200, .5);
+        } else {
+            box.style.opacity = .5;
+        }
         visible = true;
     };
 
     var hide = function () {
-        $box.fadeTo(200, 0);
+        if ($) {
+        	$(box).fadeTo(200, 0);
+        } else {
+        	box.style.opacity = 0;
+        }
         visible = false;
     };
 
     function adjustSize() {
-        $box.css({
-            height: 1,
-            width: 1
-        });
-
+        box.style.height = 1;
+        box.style.width = 1;
+ 
         if (visible) {
-            $box.css({
-                height: $window.height() + "px",
-                width: $window.width() + "px"
-            });
+            box.style.height: window.innerHeight + "px",
+            box.style.width: window.innerWidth + "px"
         }
     }
 
@@ -47,45 +53,43 @@ var AutoLightBox = function(elem) {
         if (elem) {
             incrementControl();
 
-            if ($parent) {
-                $parent.removeClass("lightbox-auto-control-" + (control - 1));
-                $box.removeClass("lightbox-" + (control - 1));
+            if (parent) {
+                parent.classList.remove("lightbox-auto-control-" + (control - 1));
+                box.classList.remove("lightbox-" + (control - 1));
             }
 
-            $parent = $(elem);
+            parent = elem;
 
-            $parent.addClass("lightbox-auto-control-" + control);
-            $box.addClass("lightbox-" + control);
+            parent.classList.add("lightbox-auto-control-" + control);
+            box.classList.add("lightbox-" + control);
 
-            var z = $(elem).css("z-index");
+            var z = elem.style.zIndex;
             if (!z) {
                 z = 50;
-                $(elem).css("z-index", z);
+                elem.style.zIndex = z;
             }
 
-            $box.css("z-index", parseInt(z) - 1);
+            box.style.zIndex = (parseInt(z) - 1);
         }
     }
 
     var attach = function () {
         if (!attached) {
-            $body.append($box);
+            document.body.appendChild(box);
             attached = true;
         }
     };
 
     var detach = function () {
-        $box.detach();
+        document.body.removeChild(box);
         attached = false;
     };
 
-    $box.css({
-        position: "fixed",
-        "background-color": "black",
-        opacity: 0,
-        left: 0,
-        top: 0
-    });
+    box.style.position = "fixed";
+    box.style.backgroundColor = "black";
+    box.style.opacity = 0;
+    box.style.left = 0;
+    box.style.top = 0;
 
     parent(elem);
     adjustSize();
@@ -94,11 +98,14 @@ var AutoLightBox = function(elem) {
 
     window.setInterval(function () {
         if (automatic) {
-            if ($(".lightbox-auto-control-" + control + ":visible").length > 0 && !visible) {
-                show();
-            }
-            if ($(".lightbox-auto-control-" + control + ":visible").length === 0 && visible) {
-                hide();
+            var ac = document.querySelector(".lightbox-auto-control-" + control);
+            if (ac && ac.length > 0) {
+                var style = window.getComputedStyle(ac[0]);
+                if (style.display === 'none' || style.visibility === 'hidden' || style.opacity === 0){
+                    if (visible) hide();
+                } else {
+                    if (!visible) show();
+                }
             }
         }
 
@@ -116,9 +123,7 @@ var AutoLightBox = function(elem) {
         detach: detach,
         parent: parent,
         color: function (c) {
-            $box.css({
-                "background-color": c
-            });
+            box.style.backgroundColor = c;
         },
         automatic: automatic
     }
